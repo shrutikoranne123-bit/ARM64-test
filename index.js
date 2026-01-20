3cb400fe47821eb4b660a6de819c45dd86a6a7e5
Shruti Koranne <shrutikoranne123@gmail.com>
2026-01-20 12:14:15 +0530

    Final Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from ARM64 Docker!\n This is Shruti\n");
+  res.end("Hello from ARM64 Docker!\n");
 });
 
 server.listen(3000, () => {
